01742662f0cdfcd0d3cc13bf35add2f458d5b449
jagadishck018 <jagadishck.018@gmail.com>
2025-03-26 10:22:54 +0530

    add JWT secret key configuration, update Post model with userId, and set up routing

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,10 @@
 import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { allRoutes } from './routes/routes'
+import "dotenv/config"
 
-const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
+
+serve(allRoutes,(info) => {
   console.log(`Server is running on http://localhost:${info.port}`)
 })
