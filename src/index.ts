ead94b7d3682049d4a322902111724659b143bd9
jagadishck018 <jagadishck.018@gmail.com>
2025-04-05 11:48:54 +0530

    feat: add Swagger UI integration and OpenAPI documentation
    - Added @hono/swagger-ui and swagger-ui dependencies to package.json.
    - Created swagger-routes.ts to serve Swagger UI and OpenAPI JSON.
    - Integrated Swagger routes into the main application routes.
    - Added OpenAPI specification in openapi.ts for API documentation.
    - Updated index.ts to ensure proper server setup.

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ if (process.env.NODE_ENV !== "production") {
 import { serve } from "@hono/node-server";
 import { allRoutes } from "./routes/routes";
 
+
 serve(allRoutes, (info) => {
   console.log(`SERVER RUNNING ON PORT ${info.port}`);
 });
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -5,6 +5,7 @@ import { postsRoutes } from "./post-routes";
 import { likeRoutes } from "./likes-routes";
 import { commentRoutes } from "./comments-routes";
 import { logger } from "hono/logger";
+import { swaggerRoutes } from "./swagger-routes";
 
 export const allRoutes = new Hono();
 allRoutes.use(logger());
@@ -14,3 +15,4 @@ allRoutes.route("/users", usersRoutes);
 allRoutes.route("/posts", postsRoutes);
 allRoutes.route("/likes", likeRoutes);
 allRoutes.route("/comments", commentRoutes);
+allRoutes.route("/ui", swaggerRoutes);
